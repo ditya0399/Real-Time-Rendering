80ae7138ffbfe56629eda390c799ac9304b6c0dc
ditya0399 <adiboob@gmail.com>
2020-08-30 17:11:27 +0530

    added relaational operators assignments

---
+++ ProgrammablePipeline/EXTRA/C/Operators/RelationalOperators/main.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+
+void main()
+{
+    //variables 
+    int a;
+    int b;
+    int result;
+
+    //code
+    printf("Enter one integer:");
+    scanf("%d",&a);
+
+    printf("\n\n");
+    printf("Enter Another Integer: ");
+    scanf("%d",&b);
+
+    printf("\n\n");
+    printf("If Answer is 0,It is FALSE.\n");
+    printf("If Answer is 1,It is TRUE.\n");
+
+    result = (a < b);
+    printf("(a < b) A = %d  and B = %d  and result is %d\n",a,b,result);
+    
+
+
+
+
+
+}
