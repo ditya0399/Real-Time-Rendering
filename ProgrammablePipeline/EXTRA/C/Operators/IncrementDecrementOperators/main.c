a2fcfc8ad7cdb59829daa449576a8d699829f74f
ditya0399 <adiboob@gmail.com>
2020-08-30 16:19:51 +0530

    C - Increment and Decrement Operator assignment

---
+++ ProgrammablePipeline/EXTRA/C/Operators/IncrementDecrementOperators/main.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+
+void main()
+{
+    //code 
+
+    int a  = 5;
+    int b  = 10;
+
+    printf("A = %d\n",a);
+    printf("A = %d\n",a++);
+    printf("A = %d\n",a);
+    printf("A = %d\n\n",++a);
+
+
+    printf("B = %d\n", b);
+	printf("B = %d\n", b--);
+	printf("B = %d\n", b);
+	printf("B = %d\n\n", --b);
+
+  
+
+}
